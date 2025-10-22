9c659ec458f93de3329b1da7afe0ddbb504a2e46
Thamili2025 <thamili.delgado@escola.pr.gov.br>
2025-10-22 00:57:32 +0000

    k

---
+++ perguntas.js
@@ -0,0 +1,70 @@
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'História',
+    'Quem foi o líder da Revolução Francesa?',
+    'Maximilien Robespierre.'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Qual é a origem da língua inglesa?',
+    'A origem da língua inglesa é fascinante e complexa, resultando de uma mistura de várias línguas ao longo dos séculos.'
+)
+criaCartao(
+    'Portugues',
+    'Qual é a origem da língua portuguesa?',
+    'A língua portuguesa tem uma origem rica e complexa, resultante de uma combinação de influências de línguas antigas e de processos históricos que ocorreram ao longo de séculos.'
+)
+
+criaCartao(
+    'quimica',
+    'O que é a configuração eletrônica e qual é a configuração eletrônica do elemento oxigênio (O)?',
+    'A configuração eletrônica descreve como os elétrons estão distribuídos nas camadas ou orbitais de um átomo.'
+)
+
+criaCartao(
+    'Biologia ',
+    'Qual é a principal função das mitocôndrias na célula?',
+    'As mitocôndrias são conhecidas como as "usinas de energia" da célula, pois sua principal função é produzir energia na forma de ATP (adenosina trifosfato) por meio do processo de respiração celular.'
+)
+
+criaCartao(
+    'EDucação Financeira',
+    'Você tem R$ 5.000,00 disponíveis para investir e deseja comprar um carro no valor de R$ 20.000,00 em 2 anos. Qual seria a contribuição mensal necessária para atingir o valor total, considerando um rendimento de 0,8% ao mês (juros compostos)?',
+    'Considerando que você já possui R$ 5.000,00 iniciais e os juros compostos do investimento são de 0,8% ao mês.'
+)
+
+criaCartao(
+    'Projeto de vida',
+    'Como a definição de metas de curto, médio e longo prazo pode ajudar no desenvolvimento de um projeto de vida?',
+    'Definir metas de curto, médio e longo prazo é uma das estratégias mais eficazes para o desenvolvimento de um projeto de vida, pois ajuda a dar clareza e direção aos nossos objetivos e ações.'
+)
+
+criaCartao(
+    'Biologia 1',
+ 'Qual é a função dos ribossomos nas células?',
+    'Os ribossomos são estruturas responsáveis pela síntese de proteínas nas células. Eles podem estar livres no citoplasma ou aderidos ao retículo endoplasmático rugoso.'
+)
+
+criaCartao(
+    'Sociologia ',
+    'O que é estratificação social e como ela se manifesta nas sociedades modernas?',
+    'estratificação social refere-se à forma como a sociedade está organizada em camadas ou "estratos" sociais, com base em critérios como riqueza, poder, prestígio e acesso a recursos.'
+)
+
+criaCartao(
+    'Matematica 1',
+    'Qual é a diferença entre números racionais e números irracionais?',
+    'Racionais: Podem ser expressos como frações. Irracionais: Não podem ser expressos como frações e têm decimais infinitos e não periódicos.'
+)
